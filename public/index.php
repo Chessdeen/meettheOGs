c3e307636e1605a67fb0f4ff7eef69c007076057
Adeola Mubarak <37848504+Chessdeen@users.noreply.github.com>
2023-02-17 14:04:34 +0100

    new assets

---
+++ public/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Meet The OGs">
+    <meta name="generator" content="meettheOGs">
+    <title>Cover Template · Bootstrap v5.3</title>
+
+    <link rel="canonical" href="">
+
+   
+  <link href="styles.css" rel="stylesheet">
+  
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/dist/css/style.css" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/c877ae4731.js" crossorigin="anonymous"></script>
+
+  </head>
+  <body class="d-flex h-100 text-center text-bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">MeetTheOgs</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="index.php"><i class="fa-solid fa-house"></i></a>
+        <a class="nav-link fw-bold py-1 px-0" href="about.php"><i class="fa-solid fa-heart"></i></a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+        <a class="nav-link fw-bold py-1 px-0" href="login.php" style="width:18;height:16;"><i class="fa-solid fa-right-to-bracket"></i></a>
+        
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+  <h1 class="text-3xl font-bold underline">
+    MEET THE OG'S
+  </h1>
+    <!-- <h1>Cover your page.</h1> -->
+    <p class="lead">Love is a beautiful thing. So we met minds to create a memory to never forget by redesigning what it means to celebrate love.</p>
+    <!-- <p class="lead">
+      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
+    </p> -->
+    <button class="px-4 py-1 text-sm text-lilac-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">RSVP</button>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>&#169;  <?php   echo date("Y"); ?>  Meet The Oginni's, made with <i class="fa-solid fa-heart" style="color:red;"></i> by <a href="https://ade.codes" class="text-white" target="_blank">Ade</a>.</p>
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.php'
+  ],
+  // ...
+}
